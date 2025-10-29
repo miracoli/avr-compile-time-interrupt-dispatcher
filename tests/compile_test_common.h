e97988c7e420e4a834526185c29430f1dcdcb1f1
Dirk Petrautzki <dirk@dirk-petrautzki.de>
2025-10-29 08:37:44 +0100

    Add relative jump compile test and extend CI checks

---
+++ tests/compile_test_common.h
@@ -0,0 +1,53 @@
+#ifndef COMPILE_TEST_COMMON_H_
+#define COMPILE_TEST_COMMON_H_
+
+#include <avr/interrupt.h>
+#include <avr/io.h>
+
+#include "InterruptDispatcher.h"
+
+int main();
+
+ISR(BADISR_vect) {
+  while (true) {
+  }
+}
+
+extern "C" void __dtors_end();
+
+extern "C" void reset(void) __attribute__((used, naked));
+
+extern "C" void reset(void) {
+  asm("clr r1");
+  SREG = 0;
+  SP = RAMEND;
+  goto *&__dtors_end;
+}
+
+extern "C" void _exit();
+
+extern "C" void jmp_main(void) __attribute__((used, naked, section(".init9")));
+
+extern "C" void jmp_main(void) {
+  main();
+  goto *&_exit;
+}
+
+class DummyHandler {
+ public:
+  static constexpr bool canHandleVectNum(unsigned int vectNum) {
+    return vectNum == 1;
+  }
+  __attribute__((noreturn)) static void __vector() {
+    asm volatile(
+        "1:   nop       \n\t"
+        "     nop       \n\t"
+        "     rjmp 1b   \n\t"
+        :
+        :
+        : "memory");
+    __builtin_unreachable();
+  }
+};
+
+#endif  // COMPILE_TEST_COMMON_H_
--- tests/verify_vector_table.py
+++ tests/verify_vector_table.py
@@ -126,7 +126,7 @@ def locate_vector_bounds(binary_path: Path) -> tuple[int, int]:
 
 
 _JMP_PREFIX_RE = re.compile(
-    r"^\s*([0-9a-fA-F]+):\s+(?:[0-9a-fA-F]{2}\s+){2,}\s+jmp\b", re.IGNORECASE
+    r"^\s*([0-9a-fA-F]+):\s+(?:[0-9a-fA-F]{2}\s+){2,}\s+r?jmp\b", re.IGNORECASE
 )
 
 
